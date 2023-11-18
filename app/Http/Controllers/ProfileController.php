844f3956b61885a4be372ee87d678a8b6e94d3df
tanvirtazulislam <tanvirtazul@gmail.com>
2023-11-18 22:29:45 +0600

    Module 8 Assignment - Ostad

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,62 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+ 
+
+class ProfileController extends Controller
+
+
+{
+
+
+    public function index($id = ''){
+
+        $name = 'Donal Trump';
+
+
+        $age = '75';
+
+
+
+        $data = [
+            'id'=>$id,
+            'name'=>$name,
+            'age'=>$age
+        ];
+
+
+    
+
+
+        $name = 'access_token';
+
+
+        $value = '123-XYZ';
+
+
+        $minutes = 1;
+
+
+        $path = '/';
+
+
+        $domain = $_SERVER['SERVER_NAME'];
+
+
+        $secure = false;
+
+
+        $httpOnly = true;
+
+
+
+
+        return response($data, status:200)->cookie($name, $value, $minutes, $path, $domain, $secure, $httpOnly);
+
+
+    }
+
+}
+
